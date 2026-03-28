4e9b987090ab6c77ec9540a05a0a5ed26062a86d
Aridam9701 <arturopalacioslima@gmail.com>
2026-03-27 22:18:14 -0600

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  safelist: [
+  'bg-blue-500/20','text-blue-400',
+  'bg-cyan-500/20','text-cyan-400',
+  'bg-green-500/20','text-green-400',
+  'bg-yellow-500/20','text-yellow-400',
+  'bg-red-500/20','text-red-400',
+],
   theme: {
     extend: {},
   },
